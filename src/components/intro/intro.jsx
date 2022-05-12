1e3b2872376783d31d0462484b82b3f038278b89
dominique <domin@DESKTOP-OT2687Q>
2022-05-12 18:08:25 -0500

    title page

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,22 @@
+import "./intro.css"
+
+const Intro = () => {
+    return (
+        <div className="intro"> 
+        
+        <div className="intro-left-wrapper">
+            <h2 className="i-intro">Hello, My name is </h2>
+            <h1 className="intro-name">Dominique Akers</h1>
+            <div className="intro-title">
+                <div className="intro-title-wrapper">
+                    <div className="intro-title-item">Full stack Web Developer</div>
+                    <div className="intro-title-item">UI/UX</div>
+                    <div className="intro-title-item">Content Creator</div>
+                </div>
+            </div>
+        </div>
+        <div className="intro-right">right</div>
+            </div>
+    )
+}
+export default Intro
