253d6ca92fc362e2264c1bb5ccda0424304bddec
Dominique Akers <93212822+Dommy99@users.noreply.github.com>
2023-06-04 22:49:22 -0500

    update

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,23 +1,30 @@
 import Nav from "react-bootstrap/Nav";
-import Navbar from 'react-bootstrap/Navbar'
-import Container from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Nav";
 import "./header.css";
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-
-<Navbar bg="dark" variant="dark" fixed="top" >
-    <Container className="nav">
-    <Navbar.Brand >Dominique Akers</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#Portfolio"><Link to="/Portfolio" className="linkText">Portfolio</Link></Nav.Link>
-      <Nav.Link href="#About"><Link to="/About" className="linkText">About</Link></Nav.Link>
-      <Nav.Link href="#Resume"><Link to="/Resume" className="linkText">Resume</Link></Nav.Link>
-      <Nav.Link href="#Contact"><Link to="/Contact" className="linkText">Contact</Link></Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
+    <Navbar bg="dark" variant="dark" fixed="top">
+      <Container className="nav">
+        <Navbar.Brand>Dominique Akers</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/Portfolio" className="linkText">
+            Portfolio
+          </Nav.Link>
+          <Nav.Link as={Link} to="/About" className="linkText">
+            About
+          </Nav.Link>
+          <Nav.Link as={Link} to="/Resume" className="linkText">
+            Resume
+          </Nav.Link>
+          <Nav.Link as={Link} to="/Contact" className="linkText">
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
